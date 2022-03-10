64c45ac99400167a1f0c5e7592906718197ca066
Derek <nightwing917@hotmail.com>
2022-03-09 19:35:50 -0700

    adding winner

--- machine.js
+++ machine.js
@@ -1,6 +1,7 @@
 class Machine extends React.Component {
     render() {
         const {s1,s2,s3} = this.props;
+        const winner = (s1 === s2) && (s2 === s3);
         return (
             <div>
                 <p>{s1} {s2} {s3}</p>
