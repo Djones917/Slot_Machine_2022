2800075138e18a1a5a96332b6bf751b6b9430e0a
Derek <nightwing917@hotmail.com>
2022-03-15 18:05:43 -0800

    added font size to p

--- machine.js
+++ machine.js
@@ -4,7 +4,9 @@ class Machine extends React.Component {
         const winner = (s1 === s2) && (s2 === s3);
         return (
             <div className="Machine">
-                <p>{s1} {s2} {s3}</p>
+                <p style={{fontSize: '50px'}}>
+                    {s1} {s2} {s3}
+                </p>
                 <p>{winner ? 'Winner!' : 'Loser!'}</p>
             </div>
         )
